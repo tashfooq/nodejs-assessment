387e864d1eb1fc9029512a67db1cf5645b78fd59
Tashfiq Chowdhury <tshfq.chw@gmail.com>
2024-06-03 13:40:50 -0700

    using dotenv to store information like port fixing some imports in index.ts fixing error handling for app.listen

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,30 @@
-global.PORT = 3000;
+import express from "express";
+import * as dotenv from "dotenv";
 
-const express = require('express');
+dotenv.config();
+const port = process.env.PORT || 3000;
 const app = express();
+const json = express.json();
+app.use(json);
 
-const userController = require('./usercontroller');
+const userController = require("./usercontroller");
 
-app.get('/users', userController.getAllUsers);
-app.post('/users', userController.createUser);
-app.get('/users/:id', userController.getUserById);
-app.put('/users/:id', userController.updateUser);
-app.delete('/users/:id', userController.deleteUser);
+app.get("/users", userController.getAllUsers);
+app.post("/users", userController.createUser);
+app.get("/users/:id", userController.getUserById);
+app.put("/users/:id", userController.updateUser);
+app.delete("/users/:id", userController.deleteUser);
 
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.status(200).send("<h1>Hello World</h1>");
+});
 
-app.get('/', (req,res) => {
-	res.set('Content-Type', 'text/html');
-	res.status(200).send('<h1>Hello World</h1>')
-})
+app.listen(port, (error?: Error) => {
+  if (!error) {
+    console.info(`App successfully started and is listening on port ` + port);
+    return;
+  }
 
-app.listen(global.PORT, (error,) => {
-	if (!error) {
-		console.info(`App successfully started and is listening on port ` + global.PORT)
-		return;
-	}
-	
-	console.info(`Error occurred, server can't start`, error)
-})
+  console.error(`Error occurred, server can't start`, error);
+});
