e4f0666918fe3879a8a99cc482c024fe3ab686a5
Tashfiq Chowdhury <tshfq.chw@gmail.com>
2024-06-03 14:03:05 -0700

    using router for user routes starting moving some logic out of the controller into the user

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import userRoutes from "./userRoutes";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -7,14 +8,7 @@ const app = express();
 const json = express.json();
 app.use(json);
 
-const userController = require("./usercontroller");
-
-app.get("/users", userController.getAllUsers);
-app.post("/users", userController.createUser);
-app.get("/users/:id", userController.getUserById);
-app.put("/users/:id", userController.updateUser);
-app.delete("/users/:id", userController.deleteUser);
-
+app.use("/users", userRoutes);
 app.get("/", (req, res) => {
   res.set("Content-Type", "text/html");
   res.status(200).send("<h1>Hello World</h1>");
---
+++ src/userRoutes.ts
@@ -0,0 +1,10 @@
+import express, { Router } from "express";
+const router = express.Router();
+
+router.get("/", userController.getAllUsers);
+router.post("/", userController.createUser);
+router.get("/:id", userController.getUserById);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+
+export default router;
